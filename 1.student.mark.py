35522ab82460d7307702ad14dd0674d8ec468de2
Vinh <vinhnd.bi12-491@st.usth.edu.vn>
2023-03-07 01:23:12 +0700

    the first end ver #might change later

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,132 +1,199 @@
-"""
-python program 1?  
-"""                 #need to use function(def) :D
-
-numStudent = 0
-while numStudent == 0:
+# Students input
+def input_num_students():       #return number of student
     nhap = input('Input number of students in the class: ')
     if nhap.isnumeric():
-        if int(nhap)>10:
-            print('Are you sure?')
         numStudent = int(nhap)
     else:
-        print('please type in a number')
+        input_num_students()
+    return numStudent
 
-#print('There are '+ str(numStudent) +' students in the class')
+def input_students_info(x):     #return list of students
+    students = []
+    studentId = []
+    studentName = []
+    studentDoB = []
+    for i in range(int(x)):
+        studentId.append(i)
+        studentName.append(i)
+        studentDoB.append(i)
+        
+        print('Input the Id, name and DoB of the \"'+ str(i+1) +'\" student') 
+        studentId[i] = (input('Id:'))
+        studentName[i] = str(input('Name:'))
+        studentDoB[i] = str(input('Date:'))
 
-students = []
-studentId = []
-studentName = []
-studentDoB = []
+        student = {}
+        student["Id"] = studentId[i]
+        student["Name"] = studentName[i] 
+        student["DoB"] = studentDoB[i]
 
-for i in range(numStudent):
-    studentId.append(i)
-    studentName.append(i)
-    studentDoB.append(i)
-    
-    print('Input the Id, name and DoB of the \"'+ str(i+1) +'\" student') 
-    studentId[i] = (input('Id:'))
-    studentName[i] = str(input('Name:'))
-    studentDoB[i] = str(input('Date:'))
+        students.append(student)
+        del student                 #not necessary because python clear it?
+    return students
 
-    student = {}
-    student["Id"] = studentId[i]
-    student["Name"] = studentName[i] 
-    student["DoB"] = studentDoB[i]
+def print_students_info(x):     #feed list student in
+    print('Select a number to print: ')
+    print('1: List all students and information')
+    for i in range(3):
+        key_student = list(x[0].keys())
+        print(str(i+2)+ ": "+ key_student[i])
 
-    students.append(student)
-    del student                 #not necessary because python clear it?
+    y = 0
+    while y == 0:
+        nhap = input('enter number in the given list: ')
+        if nhap.isnumeric():
+            if int(nhap) in range(1,4):
+                y = int(nhap)
+        else:
+            print('please type in a number')
 
-print('Select a number to print: ')
-print('1: List all student and information')
-for i in range(3):
-    key_student = list(students[0].keys())
-    print(str(i+2)+ ": "+ key_student[i])
-
-x = 0
-while x == 0:
-    nhap = input('enter number in the given list: ')
-    if nhap.isnumeric():
-        if int(nhap) in range(1,4):
-            x = int(nhap)
+    if y==1:
+        print(x)
+    elif y==2:
+        for i in range(len(x)):
+            print(x[i]['Id'])        
+    elif y==3:
+        for i in range(len(x)):
+            print(x[i]['Name'])
     else:
-        print('please type in a number')
-
-if x==1:
-    print(students)
-elif x==2:
-    print(studentId)
-elif x==3:
-   print(studentName)
-else:
-    print(studentDoB)
+        for i in range(len(x)):
+            print(x[i]['DoB'])        
+    
+    return 0
 
 ''' code check'''  #codeCheck #codeCheck #codeCheck #codeCheck #codeCheck #codeCheck 
-
+# i need this but how to minimal ?
 '''print('all students:',end=' ')
-print(students)
 print(type(students))
-
-print(students[0])
 print(type(students[0]))
 
 print(students[0]['Name'], students[0]['Id'])
 print(type(students[0]['Name']))
-'''
+
 """print(student)       #already deleted(del)
 print(type(student))""" #this changed after each loop, only use studentS
-
-'''print(studentId)
-print(studentId[0])
-
-print(studentName)
-print(studentName[0])'''
-
-'''               #codeCheck #codeCheck #codeCheck #codeCheck #codeCheck #codeCheck 
+     
 leTroll 
-'''
+'''                       #codeCheck #codeCheck #codeCheck #codeCheck #codeCheck #codeCheck 
 
-numCourse = 0
-while   numCourse == 0:
+# Input courses
+def input_num_courses():    # return number of courses
     nhap = input('Input number of courses in this class: ')
     if nhap.isnumeric():
         numCourse = int(nhap)
     else:
-        print('Plz enter a number')
+        input_num_courses()
+    return numCourse
 
-courseId = []
-courseName = []
-courses = []
+def input_courses_info(x):  #return list of courses; need to eat an int
+    courses = []
+    courseId = []
+    courseName = []
+    for i in range(int(x)):
+        courseId.append(i)
+        courseName.append(i)
 
-for i in range(numCourse):
-    courseId.append(i)
-    courseName.append(i)
+        print('Input the id and the name of the \"'+ str(i+1)+'\" course')
+        courseId[i] = (input('Course Id:'))
+        courseName[i] = str(input('Course name:'))
 
-    print('Input the id and the name of the \"'+ str(i+1)+'\" course')
-    courseId[i] = (input('Course Id:'))
-    courseName[i] = str(input('Course name:'))
+        course = {'id': courseId[i], 'name': courseName[i]}
+        
+        courses.append(course)
+        del course                            
+    return courses
 
-    course = {'id': courseId[i], 'name': courseName[i]}
-    
-    courses.append(course)
-    del course                            
+def print_courses_info(x):  #return info, need to eat course list
+    print('Select a number to print: ')
+    print('1: List information of all courses')
+    for i in range(2):
+        key_courses = list(x[0].keys())
+        print(str(i+2)+ ": "+ key_courses[i])
 
-'''
-print(courses)
-print(courseId)
-print(courseName)
-'''
-print('Select a course to input students\' marks')
-for i in range(numCourse):
-    print(str(i+1)+ ": "+ courseName[i])
-
-x = 0
-while   x == 0:
-    nhap = input('select a course: ')
-    if nhap.isnumeric():
-        if (int(nhap)) in range(1,numCourse+1):
-            x = int(nhap)
+    y = 0
+    while y == 0:
+        nhap = input('enter number in the given list: ')
+        if nhap.isnumeric():
+            if int(nhap) in range(1,3):
+                y = int(nhap)
         else:
-            print('form 1 to '+ str(numCourse))
+            print('please type in a number')
+
+    if y==1:
+        print(x)
+    elif y==2:
+        for i in range(len(x)):
+            print(x[i]['id'])
     else:
-        print('Plz enter a number in the given list')
+        for i in range(len(x)):
+            print(x[i]['name'])
+    return 0
+
+def input_marks(a,b,c):     # a is numcourse; b is list of courses; c is list students
+    print('Select a course to input students\' marks')
+    a= int(a)
+    for i in range(a):
+        print(str(i+1)+ ": "+ b[i]['name'])
+
+    x = 0
+    while   x == 0:
+        nhap = input('select a course: ')
+        if nhap.isnumeric():
+            if (int(nhap)) in range(1,a+1):
+                x = int(nhap)
+            else:
+                print('form 1 to '+ str(a))
+        else:
+            print('Plz enter a number in the given list')
+    
+    print("Enter points of course "+ b[x-1]['name'])
+    L = []
+    for i in range(len(c)):
+        mark = int(input('mark of '+ c[i]['Name']+ ' :' ))
+
+        S = {'name': c[i]['Name'], 'mark': int(mark)}
+        L.append(S)
+        del S
+
+    b[x-1]['mark'] = L
+    return b
+
+def show_marks(a):          # a is list of courses with marks
+    for i in range(len(a)):
+        print(str(i+1) + ': ' + a[i]['name'])  
+    print('select course to output marks:')
+    
+    y = 0
+    while y == 0:
+        nhap = input('enter number in the given list: ')
+        if nhap.isnumeric():
+            if int(nhap) in range(1,len(a)+1):
+                y = int(nhap)
+        else:
+            print('please type in a number')
+
+    if 'mark' in a[y-1]:
+        print(a[y-1]['mark'])
+    
+    return 0
+
+#main func:
+
+numstu = input_num_students()
+
+student_list = input_students_info(numstu)
+
+print_students_info(student_list)
+
+numCours= input_num_courses()
+
+course_list = input_courses_info(numCours)
+
+print_courses_info(course_list)
+
+courseList_w_Marks = input_marks(numCours,course_list,student_list)
+
+show_marks(courseList_w_Marks)
+
+print("Done input students, courses and marks!")
+
